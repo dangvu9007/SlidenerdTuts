759a84a3cc767d651a8059bd8f7de46230ed3671
zandoli <vaughnaimable@gmail.com>
2015-03-30 14:10:25 -0400

    Initial

---
+++ src/Tut26CalculateOvertimeBonus.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Tut26CalculateOvertimeBonus {
+    public static void main(String[] args)throws IOException{
+
+        BufferedReader reader =new BufferedReader(new InputStreamReader(System.in));
+
+        double hours=0, rate=0, pay=0;
+
+        System.out.println("Enter hours worked: ");
+        hours = Double.parseDouble(reader.readLine());
+
+        System.out.println("Enter rate: ");
+        rate = Double.parseDouble(reader.readLine());
+
+        if (hours<=40) {
+
+            pay= rate*40;
+            System.out.println("Pay is: "+pay);
+        }
+
+        else {
+
+            pay= rate*40 + (hours-40)*(rate+(0.5*rate));
+            System.out.println("Pay is: " +pay);
+        }
+
+
+    }
+
+}
