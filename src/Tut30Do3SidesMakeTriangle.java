a614efd2480498dbcaf1030853ebb6ad44c58292
zandoli <vaughnaimable@gmail.com>
2015-04-01 10:43:46 -0400

    Initial

---
+++ src/Tut30Do3SidesMakeTriangle.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Tut30Do3SidesMakeTriangle {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        int a=0, b=0, c=0;
+        double p=0, temp=0, area=0;
+        System.out.println("Enter length of a; ");
+        a = Integer.parseInt(reader.readLine());
+
+        System.out.println("Enter length of b; ");
+        b = Integer.parseInt(reader.readLine());
+
+        System.out.println("Enter length of c; ");
+        c = Integer.parseInt(reader.readLine());
+
+        if (a+b>c && a+c>b && b+c>a){
+            p=a+b+c;
+            temp = p*0.5;
+            area = Math.sqrt((temp)*(temp-a)*(temp-b)*(temp-c));
+            System.out.println("This is a triangle with Perimeter "+p+ " and Area "+area);
+        }
+        else{
+            System.out.println("This is not a triangle");
+        }
+    }
+}
