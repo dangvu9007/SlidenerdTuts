c99528b0890d082173ab142f8cc63c7452c2e5f3
zandoli <vaughnaimable@gmail.com>
2015-04-02 11:25:24 -0400

    Initial

---
+++ src/Tut32BMICalculator.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Tut32BMICalculator {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        double weight=0, height=0, bmi=0;
+
+        System.out.println("Enter weight in kg: ");
+        weight = Double.parseDouble(reader.readLine());
+
+        System.out.println("Enter height in cm: ");
+        height = Double.parseDouble(reader.readLine());
+        height = height/100;
+
+        bmi = weight/(height*height);
+
+        if (bmi<18.5){
+            System.out.println("Your BMI is "+bmi+" and you are underweight");
+        }
+        else if (bmi>=18.5 && bmi<25){
+            System.out.println("Your BMI is "+bmi+" and you are normal");
+        }
+        else if (bmi>=25 && bmi<30){
+            System.out.println("Your BMI is "+bmi+" and you are overweight");
+        }
+        else if (bmi>30){
+            System.out.println("Your BMI is "+bmi+" and you are obese");
+        }
+        else{
+            System.out.println("Incorrect BMI");
+        }
+
+    }
+}
