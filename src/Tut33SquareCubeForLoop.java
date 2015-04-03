17cecd05488fb9fb2c81c38a6a2009fa1927c695
zandoli <vaughnaimable@gmail.com>
2015-04-03 09:51:04 -0400

    Initial

--- src/Tut33SquareCubeForLoop.java
+++ src/Tut33SquareCubeForLoop.java
@@ -1,10 +1,5 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-
 public class Tut33SquareCubeForLoop {
     public static void main(String[] args){
-
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
         for (int i=2; i<=40; i+=2){
 
