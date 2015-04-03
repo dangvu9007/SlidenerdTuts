4ee00327c2fef2e88b8bd872d91943350a86a77c
zandoli <vaughnaimable@gmail.com>
2015-04-03 10:09:55 -0400

    Initial

---
+++ src/Tut34SumForLoop.java
@@ -0,0 +1,8 @@
+public class Tut34SumForLoop {
+    public static void main(String[] args){
+        int y=0;
+        for(int i=1; i<=10; i++){
+            y= y+i;
+        }System.out.println("Sum of first 10 numbers is "+y);
+    }
+}
