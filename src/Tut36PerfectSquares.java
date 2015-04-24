f7674c4d7416cde24c26bac76a47d4d3e75e29ad
zandoli <vaughnaimable@gmail.com>
2015-04-24 19:22:28 -0400

    Initial

---
+++ src/Tut36PerfectSquares.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Tut36PerfectSquares {
+    public static void main(String[] args)throws IOException {
+
+        //BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        //System.out.println("Enter number: ");
+        //int num = Integer.parseInt(reader.readLine());
+
+        for (int i = 1; i <= 100; i++) {
+
+            int num = i;
+            double sq = Math.sqrt(num);
+
+            double round = Math.ceil(sq);
+
+            if (sq == round) {
+                System.out.println(num+" is a Perfect Square");
+            }
+            //else {
+                //System.out.println(num+" is not a Perfect Square");
+            //}
+
+        }
+    }
+}
