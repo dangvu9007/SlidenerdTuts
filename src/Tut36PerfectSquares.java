aed7096ac9e4036ed3dbc54496244ca6a480e63d
zandoli <vaughnaimable@gmail.com>
2015-05-23 17:00:25 -0400

    Initial

--- src/Tut36PerfectSquares.java
+++ src/Tut36PerfectSquares.java
@@ -11,13 +11,20 @@ public class Tut36PerfectSquares {
 
         for (int i = 1; i <= 100; i++) {
 
+            /*
             int num = i;
             double sq = Math.sqrt(num);
-
             double round = Math.ceil(sq);
 
             if (sq == round) {
                 System.out.println(num+" is a Perfect Square");
+            */
+
+            double sq = Math.sqrt(i);
+            double round = Math.ceil(sq);
+
+            if (sq == round) {
+                System.out.println(i+" is a Perfect Square");
             }
             //else {
                 //System.out.println(num+" is not a Perfect Square");
