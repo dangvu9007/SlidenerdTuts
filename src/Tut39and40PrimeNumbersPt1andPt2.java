b20f109a3cc11cc087d80a0044d28154daf50744
zandoli <vaughnaimable@gmail.com>
2015-06-28 09:11:13 -0400

    Initial

--- src/Tut39and40PrimeNumbersPt1andPt2.java
+++ src/Tut39and40PrimeNumbersPt1andPt2.java
@@ -1,9 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Tut39and40PrimeNumbersPt1andPt2 {
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("Enter number: ");
+        int num = Integer.parseInt(reader.readLine());
 
         int cnt=0;
-        for (int n=2; n<=100; n++){
+        for (int n=2; n<=num; n++){
 
             for (int i=1; i<=n; i++){
 
